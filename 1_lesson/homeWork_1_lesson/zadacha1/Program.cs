ec38a8feda7390d13875fe488eb2fb633d42efa0
Sanek <aleksan-85@mail.ru>
2022-11-22 23:23:32 +0300

    reshenie zadacha_1

--- 1_lesson/homeWork_1_lesson/zadacha1/Program.cs
+++ 1_lesson/homeWork_1_lesson/zadacha1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (b > a)
+{
+    Console.Write("Максимальное число: ");
+    Console.Write(b);
+}
+else
+{
+    Console.Write("Максимальное число: ",a);
+    Console.Write(a);
+}
+// Console.Write($"{NegN} ");
+//string a = Console.ReadLine();
+//int N = int.Parse(a);
+//int NegN = - N;
